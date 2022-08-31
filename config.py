70ffc5fe94668eff9f6f7e4ebba607e930829d17
Yosolo1 <107810340+Yosolo1@users.noreply.github.com>
2022-08-31 15:35:13 -0400

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-admin = 'rockstar984'
-bot_token = "5511620607:AAGc355mgb3QBz6oZSev7tD9VOVZfk1z2yk"
+admin = 'Luis_Daniel_Diaz'
+bot_token = "5391544545:AAHhauKxmTOW_nlCi0O6s9fxe9o_9g0tGRs"
 
 groupid= -1001728956224
